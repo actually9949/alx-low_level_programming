e0994e71ea8a35f27a5be2d99097936ac196a807
actually9949 <youniquelad02@gmail.com>
2023-03-23 22:54:53 -0700

    print_most_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,16 +1,18 @@
-#include "holberton.h" /* assuming the existence of this header file that includes putchar */
+#include "main.h"
 
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9,
+ * except 2 and 4, followed by a new line
+ */
 void print_most_numbers(void)
 {
-  int i;
+	int i;
 
-  for (i = 0; i < 10; i++)
-  {
-    if (i != 2 && i != 4)
-    {
-      _putchar(i + '0'); /* add '0' to convert integer to ASCII character */
-    }
-  }
-  _putchar('\n'); /* print a newline character after all the numbers are printed */
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
+	}
 }
-
