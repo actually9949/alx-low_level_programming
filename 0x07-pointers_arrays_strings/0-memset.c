4bce988b55bd08a480f32886caa4387809afe48d
actually9949 <youniquelad02@gmail.com>
2023-04-04 17:54:22 -0700

    Corrections made

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,25 +1,18 @@
+#include "main.h"
 /**
- * _memset - a function that fills memory with a constant byte
- *
- *  @s: input pointer to char type represents the pointer to the
- *  block of memory to fill.
- *
- *  @b: input variable of char type represents the character to fill
- *
- *  @n: unsigned int variable the number of bytes to be filled
- *
- *  Return: A pointer to the filled memory area @s
- */
+* _memset - fills memory with a constant byte
+* @s: pointer to the memory area to be filled
+* @b: value to be set
+* @n: number of bytes to be filled
+* Return: pointer to the memory area s
+*/
 char *_memset(char *s, char b, unsigned int n)
 {
-	/**
-	 * declare an unsigned int because we are storing a
-	 * value that will always be non-negative (zero or positive)
-	 */
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-		s[i] = b;
-
-	return (s);
+int i = 0;
+for (; n > 0; i++)
+{
+s[i] = b;
+n--;
+}
+return (s);
 }
--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,33 +1,28 @@
+#include "main.h"
 /**
- * _strspn - a function that gets the length of a prexif substring.
- *
- * @s: pointer to string input
- * @accept: substring prefix to look for
- *
- * Return: the number of bytes in the initial segment
- */
+* _strspn - gets the length of a prefix substring
+* @s: input string
+* @accept: characters to search for
+* Return: number of bytes in the initial segment of s that consist of bytes
+* from accept
+*/
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j, f;
-
-	i = 0;
-	while (s[i] != '\0')
-	{
-		j = 0;
-		f = 1; /*flag status*/
-		while (accept[j] != '\0')
-		{
-			if (s[i] == accept[j])
-			{
-				f = 0; /*success*/
-				break;
-			}
-			j++;
-		}
-		if (f == 1)
-			break;
-		i++;
-	}
-
-	return (i);
-}`
+unsigned int n = 0;
+int r;
+while (*s)
+{
+for (r = 0; accept[r]; r++)
+{
+if (*s == accept[r])
+{
+n++;
+break;
+}
+else if (accept[r + 1] == '\0')
+return (n);
+}
+s++;
+}
+return (n);
+}
